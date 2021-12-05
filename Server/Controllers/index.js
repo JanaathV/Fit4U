fe184be66fb3777d234a75f981ceb5893d4dd11a
keremduran <keremduran@live.com>
2021-12-05 13:10:45 -0500

    health data view complete

--- Server/Controllers/index.js
+++ Server/Controllers/index.js
@@ -1,144 +1,198 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DisplayContactUsPage = exports.ProcessLogoutPage = exports.ProcessRegisterPage = exports.DisplayRegisterPage = exports.ProcessLoginPage = exports.DisplayLoginPage = exports.DisplayCheckoutPage = exports.DisplayCartPage = exports.UpdateCartPage = exports.DisplayProfilePage = exports.DisplayProductPage = exports.DisplayHomePage = void 0;
-const passport_1 = __importDefault(require("passport"));
-const user_1 = __importDefault(require("../Models/user"));
-const cart_1 = __importDefault(require("../Models/cart"));
-const Util_1 = require("../Util");
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.DisplayContactUsPage =
+  exports.DisplayHealthDataPage =
+  exports.ProcessLogoutPage =
+  exports.ProcessRegisterPage =
+  exports.DisplayRegisterPage =
+  exports.ProcessLoginPage =
+  exports.DisplayLoginPage =
+  exports.DisplayCheckoutPage =
+  exports.DisplayCartPage =
+  exports.UpdateCartPage =
+  exports.DisplayProfilePage =
+  exports.DisplayProductPage =
+  exports.DisplayHomePage =
+    void 0;
+const passport_1 = __importDefault(require('passport'));
+const user_1 = __importDefault(require('../Models/user'));
+const cart_1 = __importDefault(require('../Models/cart'));
+const Util_1 = require('../Util');
 function DisplayHomePage(req, res, next) {
-    res.render('index', { title: 'Home', page: 'home', displayName: Util_1.UserDisplayName(req) });
+  res.render('index', {
+    title: 'Home',
+    page: 'home',
+    displayName: Util_1.UserDisplayName(req),
+  });
 }
 exports.DisplayHomePage = DisplayHomePage;
 function DisplayProductPage(req, res, next) {
-    res.render('index', { title: 'Product Page', page: 'productpage', displayName: Util_1.UserDisplayName(req) });
+  res.render('index', {
+    title: 'Product Page',
+    page: 'productpage',
+    displayName: Util_1.UserDisplayName(req),
+  });
 }
 exports.DisplayProductPage = DisplayProductPage;
 function DisplayProfilePage(req, res, next) {
-    res.render('index', { title: 'Profile Page', page: 'profile', displayName: Util_1.UserDisplayName(req) });
+  res.render('index', {
+    title: 'Profile Page',
+    page: 'profile',
+    displayName: Util_1.UserDisplayName(req),
+  });
 }
 exports.DisplayProfilePage = DisplayProfilePage;
 
-function DisplayCartPage(req, res, next) {
-    let query;
-    if (req.user) {
-      query = cart_1.default.findOne({ userId: req.user.id });
-    } else {
-      res.redirect("/login");
-    }
-  
-    query.exec(function (err, cart) {
-      if (err) return console.error(err);
-      if (cart) {
-        res.render("index", {
-          title: "Your Cart",
-          page: "cart",
-          displayName: Util_1.UserDisplayName(req),
-          cart,
-        });
-      } else {
-        res.redirect("/register");
-      }
-    });
-  }
-  exports.DisplayCartPage = DisplayCartPage;
+function DisplayHealthDataPage(req, res, next) {
+  res.render('index', {
+    title: 'Health Data',
+    page: 'healthData',
+    displayName: Util_1.UserDisplayName(req),
+  });
+}
+exports.DisplayHealthDataPage = DisplayHealthDataPage;
 
-  function UpdateCartPage(req, res, next) {
-    let query;
-    if (req.user) {
-      query = cart_1.default.findOne({ userId: req.user.id });
-    } else {
-      res.redirect("/login");
-    }
-    query.exec(function (err, cart) {
-      if (err) return console.error(err);
-      if (!cart) return console.error("NO CART FOUND");
-      if (req.params.cartParam == "increase") {
-        cart.increaseQuantity();
-      } else if (req.params.cartParam == "decrease" && cart.quantity > 0) {
-        cart.decreaseQuantity();
-      } else if (req.params.cartParam == "giftToggle") {
-        cart.toggleGift();
-      } else {
-        res.redirect("/cart");
-      }
-      cart.save().then(() => {
-        res.redirect("/cart");
-      });
-    });
+function DisplayCartPage(req, res, next) {
+  let query;
+  if (req.user) {
+    query = cart_1.default.findOne({ userId: req.user.id });
+  } else {
+    res.redirect('/login');
   }
-  exports.UpdateCartPage = UpdateCartPage;
+
+  query.exec(function (err, cart) {
+    if (err) return console.error(err);
+    if (cart) {
+      res.render('index', {
+        title: 'Your Cart',
+        page: 'cart',
+        displayName: Util_1.UserDisplayName(req),
+        cart,
+      });
+    } else {
+      res.redirect('/register');
+    }
+  });
+}
+exports.DisplayCartPage = DisplayCartPage;
+
+function UpdateCartPage(req, res, next) {
+  let query;
+  if (req.user) {
+    query = cart_1.default.findOne({ userId: req.user.id });
+  } else {
+    res.redirect('/login');
+  }
+  query.exec(function (err, cart) {
+    if (err) return console.error(err);
+    if (!cart) return console.error('NO CART FOUND');
+    if (req.params.cartParam == 'increase') {
+      cart.increaseQuantity();
+    } else if (req.params.cartParam == 'decrease' && cart.quantity > 0) {
+      cart.decreaseQuantity();
+    } else if (req.params.cartParam == 'giftToggle') {
+      cart.toggleGift();
+    } else {
+      res.redirect('/cart');
+    }
+    cart.save().then(() => {
+      res.redirect('/cart');
+    });
+  });
+}
+exports.UpdateCartPage = UpdateCartPage;
 
 function DisplayCheckoutPage(req, res, next) {
-    res.render('index', { title: 'Checkout', page: 'checkout', displayName: Util_1.UserDisplayName(req) });
+  res.render('index', {
+    title: 'Checkout',
+    page: 'checkout',
+    displayName: Util_1.UserDisplayName(req),
+  });
 }
 exports.DisplayCheckoutPage = DisplayCheckoutPage;
 function DisplayLoginPage(req, res, next) {
-    if (!req.user) {
-        return res.render('index', { title: 'Login', page: 'login', messages: req.flash('loginMessage'), displayName: Util_1.UserDisplayName(req) });
-    }
-    return res.redirect('/home');
+  if (!req.user) {
+    return res.render('index', {
+      title: 'Login',
+      page: 'login',
+      messages: req.flash('loginMessage'),
+      displayName: Util_1.UserDisplayName(req),
+    });
+  }
+  return res.redirect('/home');
 }
 exports.DisplayLoginPage = DisplayLoginPage;
 function ProcessLoginPage(req, res, next) {
-    passport_1.default.authenticate('local', (err, user, info) => {
-        if (err) {
-            console.error(err);
-            return next(err);
-        }
-        if (!user) {
-            req.flash('loginMessage', 'Authentication Error');
-            return res.redirect('/login');
-        }
-        req.login(user, (err) => {
-            if (err) {
-                console.error(err);
-                return next(err);
-            }
-            return res.redirect('/home');
-        });
-    })(req, res, next);
+  passport_1.default.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+    if (!user) {
+      req.flash('loginMessage', 'Authentication Error');
+      return res.redirect('/login');
+    }
+    req.login(user, (err) => {
+      if (err) {
+        console.error(err);
+        return next(err);
+      }
+      return res.redirect('/home');
+    });
+  })(req, res, next);
 }
 exports.ProcessLoginPage = ProcessLoginPage;
 function DisplayRegisterPage(req, res, next) {
-    if (!req.user) {
-        return res.render('index', { title: 'Register', page: 'register', messages: req.flash('registerMessage'), displayName: Util_1.UserDisplayName(req) });
-    }
-    return res.redirect('/home');
+  if (!req.user) {
+    return res.render('index', {
+      title: 'Register',
+      page: 'register',
+      messages: req.flash('registerMessage'),
+      displayName: Util_1.UserDisplayName(req),
+    });
+  }
+  return res.redirect('/home');
 }
 exports.DisplayRegisterPage = DisplayRegisterPage;
 function ProcessRegisterPage(req, res, next) {
-    let newUser = new user_1.default({
-        username: req.body.username,
-        emailAddress: req.body.emailAddress,
-        displayName: req.body.FirstName + " " + req.body.LastName
+  let newUser = new user_1.default({
+    username: req.body.username,
+    emailAddress: req.body.emailAddress,
+    displayName: req.body.FirstName + ' ' + req.body.LastName,
+  });
+  user_1.default.register(newUser, req.body.password, (err) => {
+    if (err) {
+      console.error('Error: Inserting New User');
+      if (err.name == 'UserExistsError') {
+        console.error('Error: User Already Exists');
+      }
+      req.flash('registerMessage', 'Registration Error');
+      return res.redirect('/register');
+    }
+    return passport_1.default.authenticate('local')(req, res, () => {
+      let newCart = new cart_1.default({ userId: req.user.id });
+      newCart.save();
+      return res.redirect('/home');
     });
-    user_1.default.register(newUser, req.body.password, (err) => {
-        if (err) {
-            console.error('Error: Inserting New User');
-            if (err.name == "UserExistsError") {
-                console.error('Error: User Already Exists');
-            }
-            req.flash('registerMessage', 'Registration Error');
-            return res.redirect('/register');
-        }
-        return passport_1.default.authenticate('local')(req, res, () => {
-            let newCart = new cart_1.default({ userId: req.user.id });
-            newCart.save();
-            return res.redirect('/home');
-        });
-    });
+  });
 }
 exports.ProcessRegisterPage = ProcessRegisterPage;
 function ProcessLogoutPage(req, res, next) {
-    req.logout();
-    res.redirect('/home');
+  req.logout();
+  res.redirect('/home');
 }
 exports.ProcessLogoutPage = ProcessLogoutPage;
 function DisplayContactUsPage(req, res, next) {
-    res.render('index', { title: 'Contact Us Page', page: 'contactus', displayName: Util_1.UserDisplayName(req) });
+  res.render('index', {
+    title: 'Contact Us Page',
+    page: 'contactus',
+    displayName: Util_1.UserDisplayName(req),
+  });
 }
 exports.DisplayContactUsPage = DisplayContactUsPage;
 //# sourceMappingURL=index.js.map
--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -1,16 +1,19 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
 const router = express_1.default.Router();
 exports.default = router;
-const index_1 = require("../Controllers/index");
+const index_1 = require('../Controllers/index');
 router.get('/', index_1.DisplayHomePage);
 router.get('/home', index_1.DisplayHomePage);
 router.get('/productpage', index_1.DisplayProductPage);
 router.get('/profile', index_1.DisplayProfilePage);
+router.get('/profile/healthData', index_1.DisplayHealthDataPage);
 router.get('/cart', index_1.DisplayCartPage);
 router.get('/updateCart/:cartParam', index_1.UpdateCartPage);
 router.get('/cart', index_1.DisplayCheckoutPage);
